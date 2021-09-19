506ddb9c36ebecc9413c897a2165a711d0c2e61a
Simon Müller <simon.mueller@student.ipso.ch>
2021-09-19 11:21:46 +0200

    CSV Reader an Writer

---
+++ CSV-Datei/CSV/CsvReader.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSV
+{
+    /// <summary>
+    /// CsvReader Klasse zum auslesen einer Csv-Datei
+    /// </summary>
+    public class CsvReader
+    {
+        /// <summary>
+        /// Pfad der Datei
+        /// </summary>
+        public string Pfad { get; private set; }
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="pfad">Pfad der Datei</param>
+        /// <exception cref="FileNotFoundException"></exception>
+        public CsvReader(string pfad)
+        {
+            if (File.Exists(pfad))
+            {
+                Pfad = pfad;
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        /// <summary>
+        /// Liest alle Linien in der Csv-Datei und gibt diese als Liste zurück.
+        /// </summary>
+        /// <returns>Liste aller Linien. Felder getrennt mit Semikolon</returns>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="System.Security.SecurityException"></exception>
+        public List<string> ReadAllLines()
+        {
+            List<string> list = new List<string>();
+            
+            foreach (var line in File.ReadAllLines(Pfad))
+            {
+                list.Add(line);
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Liest eine maximale Anzahl Linien in der Csv-Datei und gibt diese als Liste zurück.
+        /// Falls die maximale Anzahl grösser ist als die Anzahl Linien in der Datei, sowerden alle Zurückgegeben.
+        /// </summary>
+        /// <param name="max">Maximale Anzahl Linien</param>
+        /// <returns>Liste der Linien. Felder getrennt mit Semikolon.</returns>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="System.Security.SecurityException"></exception>
+        public List<string> ReadLines(int max)
+        {
+            List<string> list = new List<string>();
+            list = ReadAllLines();
+
+            if (max < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (max >= list.Count)
+            {
+                return list;
+            }
+
+            list.RemoveRange(max, list.Count - max);
+
+            return list;
+        }
+
+        /// <summary>
+        /// Liest eine maximale Anzahl Linien in der Csv-Datei ab einem index und gibt diese als Liste zurück.
+        /// Falls die maximale Anzahl grösser ist als die Anzahl Linien in der Datei, sowerden alle Zurückgegeben.
+        /// </summary>
+        /// <param name="max">Maximale Anzahl Linien</param>
+        /// <returns>Liste der Linien. Felder getrennt mit Semikolon.</returns>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        /// <exception cref="System.Security.SecurityException"></exception>
+        public List<string> ReadLines(int max, int index)
+        {
+            List<string> list = new List<string>();
+            list = ReadAllLines();
+
+            if (index == 0)
+            {
+               return ReadLines(max);
+            }
+            if(index < 0 || index >= list.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+
+            return list;
+
+        }
+
+        /// <summary>
+        /// Liest eine Linie an einem Index ein und gibt sie als String zurück.
+        /// </summary>
+        /// <param name="index">Index </param>
+        /// <returns>String mit einer Linie. Felder getrennt mit Semikolon.</returns>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public string ReadLine(int index)
+        {
+            List<string> list = new List<string>();
+            list = ReadAllLines();
+
+            if (index < 0 || index >= list.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return list[index];
+        }
+    }
+}
---
+++ CSV-Datei/CSV/CsvWriter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+namespace CSV
+{
+    /// <summary>
+    /// CsvWriter Klasse zum schreiben in eine Csv-Datei
+    /// </summary>
+    public class CsvWriter
+    {
+        /// <summary>
+        /// Pfad der Datei
+        /// </summary>
+        public string Pfad { get; private set; }
+
+        /// <summary>
+        /// Überschreiben oder nicht
+        /// </summary>
+        public bool Ueberschreiben { get; set; } = true;
+
+        /// <summary>
+        /// Konstruktor
+        /// </summary>
+        /// <param name="pfad">Pfad der Datei</param>
+        /// <exception cref="FileNotFoundException"></exception>
+        public CsvWriter(string pfad)
+        {
+            if (File.Exists(pfad))
+            {
+                Pfad = pfad;
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+        }
+
+        /// <summary>
+        /// Konstruktor mit ueberschreiber
+        /// </summary>
+        /// <param name="pfad">Pfad der Datei</param>
+        /// <param name="ueberschreiben">true = Inhalt wird überschrieben. Standard = false</param>
+        public CsvWriter(string pfad, bool ueberschreiben)
+            :this(pfad)
+        {
+            Ueberschreiben = ueberschreiben;
+        }
+
+        /// <summary>
+        /// Schreibt alle Felder in die Csv-Datei.
+        /// </summary>
+        /// <param name="felder">Alle Felder in einem Array</param>
+        /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="System.Security.SecurityException"></exception>
+        public void AddLine(string[] felder)
+        {
+            string daten = "";
+
+            foreach (var feld in felder)
+            {
+                if (feld == felder[0])
+                    daten = $"{feld}";
+                else
+                    daten += $";{feld}";
+            }
+
+            var streamWriter = new StreamWriter(Pfad, !Ueberschreiben);
+
+            streamWriter.Write(daten + "\n");
+
+            streamWriter.Close();
+        }
+
+        /// <summary>
+        /// Schreibt alle Linien im Array in die Csv-Datei.
+        /// </summary>
+        /// <param name="linien">Liste aller Linien</param>
+        /// <param name="feld_trennzeichen">Gibt an wie die Felder in jeder Linie(Jedes Array Feld) getrennt sind</param>
+        /// /// <exception cref="DirectoryNotFoundException"></exception>
+        /// <exception cref="UnauthorizedAccessException"></exception>
+        /// <exception cref="PathTooLongException"></exception>
+        /// <exception cref="System.Security.SecurityException"></exception>
+        public void AddLines(string[] linien, char feld_trennzeichen)
+        {
+            foreach (var linie in linien)
+            {
+                AddLine(linie.Split(feld_trennzeichen));
+            }
+        }
+    }
+}
